70a262c8d13caa006dfe460e702d634f369a9eff
YuliiaKozlova2703 <yutsuhako@gmail.com>
2024-10-24 10:12:39 +0200

    initial commit

--- src/main/java/demoqa/core/ApplicationManager.java
+++ src/main/java/demoqa/core/ApplicationManager.java
@@ -2,6 +2,8 @@ package demoqa.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -10,7 +12,18 @@ public class ApplicationManager {
     public WebDriver driver;
 
     protected void init() {
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser", "chrome");
+// Проверяет значение переменной browser и в зависимости от результата инициализирует нужный драйвер
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default: // Это резервный сценарий на случай, если значение browser не совпадает ни с одним из указанных случаев
+                driver = new ChromeDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // неявное// явное
         driver.get("https://demoqa.com/");
